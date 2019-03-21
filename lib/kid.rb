8dbae88fd974b5786dab5f0fc03cc2ff4dee7ee8
Kimberly Aviles <01010110.v@gmail.com>
2019-03-21 00:48:47 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -5,6 +5,8 @@ require_relative './fancy_dance.rb'
 class Kid
   include Dance
   extend MetaDancing
+  include FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
 
   attr_accessor :name
 
